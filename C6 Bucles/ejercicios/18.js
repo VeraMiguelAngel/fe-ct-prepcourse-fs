2f51f02869681a02ce83f57a2ad6ae7a623b6594
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2025-06-03 00:13:10 -0300

    ejercicios resueltos hasta el numero 20 excepto en 8 y 18

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,12 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = 1;
+
+  for(let i = Math.min(a,b); i <= Math.max(a,b); i++) {
+    producto *= i;
+  }
+  return producto;
 }
 
 module.exports = productoEntreNúmeros;
