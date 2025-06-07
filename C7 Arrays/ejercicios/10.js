c5c267f20b3b4de625b1828af7c5981b89fa15fe
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2025-06-07 00:05:33 -0300

    ejrcicios de Arrays resueltos hasta el num 11 excepto el N10(revisarlo)

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,14 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  var resultado = null;
+  array.forEach(element => {if(typeof element === 'string' && element.length > 5 && resultado === null) {
+  resultado = element;
+  }
+    
+  });
+  return resultado;
+
 }
 
 module.exports = obtenerPrimerStringLargo;
