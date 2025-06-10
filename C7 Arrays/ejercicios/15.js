39828306f4abbd7c0f70aabb733583acc2c4822f
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2025-06-09 21:01:16 -0300

    ejercicios 12 al 14, falta revisar el ej10 y el 15

--- C7 Arrays/ejercicios/15.js
+++ C7 Arrays/ejercicios/15.js
@@ -2,6 +2,20 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
-}
+// Inicializamos el índice del número más grande como 0
+  let indiceMayor = 0;
 
-module.exports = encontrarIndiceMayor;
+  // Recorremos el array a partir del segundo elemento
+  for (let i = 1; i < array.length; i++) {
+    // Si encontramos un número mayor que el actual mayor, actualizamos el índice
+    if (array[i] > array[indiceMayor]) {
+      indiceMayor = i;
+    }
+  }
+
+  // Devolvemos el valor en ese índice
+  return array[indiceMayor];
+}
+let array = [3,5,7,2,8,6];
+encontrarIndiceMayor(array);
+//module.exports = encontrarIndiceMayor;
